f136f48c458762f5bb889cff211ba78223159eb0
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2014-07-01 21:16:35 -0400

    Add a WindowsLocatorFactory to parse Windows %PATH% env var.
    Windows not only uses a different separator (';' instead of ':'), but it
    also looks in the current working directory first before checking other
    directories.
    
    This by no means handles windows support (#5), but it does close #4 as
    the path separator change is handled here.

---
+++ src/LocatorFactory/WindowsLocatorFactory.php
@@ -0,0 +1,24 @@
+<?php
+namespace Nubs\Which\LocatorFactory;
+
+use Nubs\Which\Locator;
+
+class WindowsLocatorFactory extends AbstractLocatorFactory
+{
+    /**
+     * Create a locator using a semicolon-separated PATH string.
+     *
+     * The semicolon is a hard separator.  This means that command paths cannot
+     * have a semicolon in the name.
+     *
+     * The current working directory is always prepended to the list of paths.
+     *
+     * @api
+     * @param string $path The semicolon-separated PATH string.
+     * @return \Nubs\Which\Locator The locator.
+     */
+    public function createFromPath($path)
+    {
+        return new Locator(array_merge(array('.'), array_filter(explode(';', $path))));
+    }
+}
---
+++ tests/LocatorFactory/WindowsLocatorFactoryTest.php
@@ -0,0 +1,33 @@
+<?php
+namespace Nubs\Which\LocatorFactory;
+
+use PHPUnit_Framework_TestCase;
+
+/**
+ * @coversDefaultClass \Nubs\Which\LocatorFactory\WindowsLocatorFactory
+ */
+class WindowsLocatorFactoryTest extends PHPUnit_Framework_TestCase
+{
+    private $_locatorFactory;
+
+    public function setUp()
+    {
+        $this->_locatorFactory = new WindowsLocatorFactory();
+    }
+
+    /**
+     * Verify that constructing from environment variables works.
+     *
+     * @test
+     * @covers ::createFromPath
+     * @covers ::createFromEnvironment
+     * @uses \Nubs\Which\Locator::__construct
+     */
+    public function createFromEnvironment()
+    {
+        $env = $this->getMockBuilder('\Habitat\Environment\Environment')->disableOriginalConstructor()->setMethods(array('getenv'))->getMock();
+        $env->expects($this->once())->method('getenv')->with('PATH')->will($this->returnValue('abcd'));
+
+        $locator = $this->_locatorFactory->createFromEnvironment($env);
+    }
+}
