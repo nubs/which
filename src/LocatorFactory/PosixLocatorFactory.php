939896ba2311bbe1dd391e59d713004b34972a76
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2014-07-02 20:25:48 -0400

    Reorder factory methods to place create first.

--- src/LocatorFactory/PosixLocatorFactory.php
+++ src/LocatorFactory/PosixLocatorFactory.php
@@ -11,6 +11,20 @@ use Nubs\Which\PathBuilder\PosixPathBuilder;
 class PosixLocatorFactory implements LocatorFactoryInterface
 {
     /**
+     * Create a locator from the PATH environment variable.
+     *
+     * @api
+     * @param \Habitat\Environment\Environment $environment The environment
+     *     variable wrapper.  Defaults to null which just uses PHP's built-in
+     *     getenv.
+     * @return \Nubs\Which\Locator The locator.
+     */
+    public function create(Environment $environment = null)
+    {
+        return $this->createFromPath($environment ? $environment->getenv('PATH') : getenv('PATH'));
+    }
+
+    /**
      * Create a locator using a colon-separated PATH string.
      *
      * The colon is a hard separator.  This means that command paths cannot have
@@ -23,19 +37,5 @@ class PosixLocatorFactory implements LocatorFactoryInterface
     public function createFromPath($path)
     {
         return new Locator(new PosixPathBuilder(array_filter(explode(':', $path))));
-    }
-
-    /**
-     * Create a locator from the PATH environment variable.
-     *
-     * @api
-     * @param \Habitat\Environment\Environment $environment The environment
-     *     variable wrapper.  Defaults to null which just uses PHP's built-in
-     *     getenv.
-     * @return \Nubs\Which\Locator The locator.
-     */
-    public function create(Environment $environment = null)
-    {
-        return $this->createFromPath($environment ? $environment->getenv('PATH') : getenv('PATH'));
     }
 }
--- src/LocatorFactory/WindowsLocatorFactory.php
+++ src/LocatorFactory/WindowsLocatorFactory.php
@@ -11,6 +11,23 @@ use Nubs\Which\PathBuilder\WindowsPathBuilder;
 class WindowsLocatorFactory implements LocatorFactoryInterface
 {
     /**
+     * Create a locator from the PATH and PATHEXT environment variables.
+     *
+     * @api
+     * @param \Habitat\Environment\Environment $environment The environment
+     *     variable wrapper.  Defaults to null which just uses PHP's built-in
+     *     getenv.
+     * @return \Nubs\Which\Locator The locator.
+     */
+    public function create(Environment $environment = null)
+    {
+        return $this->createFromPath(
+            $environment ? $environment->getenv('PATH') : getenv('PATH'),
+            $environment ? $environment->getenv('PATHEXT') : getenv('PATHEXT')
+        );
+    }
+
+    /**
      * Create a locator using semicolon-separated PATH and PATHEXT strings.
      *
      * The semicolon is a hard separator.  This means that command paths cannot
@@ -28,22 +45,5 @@ class WindowsLocatorFactory implements LocatorFactoryInterface
         $paths = array_merge(['.'], array_filter(explode(';', $path)));
         $extensions = array_merge([''], array_filter(explode(';', $pathext)));
         return new Locator(new WindowsPathBuilder($paths, $extensions));
-    }
-
-    /**
-     * Create a locator from the PATH and PATHEXT environment variables.
-     *
-     * @api
-     * @param \Habitat\Environment\Environment $environment The environment
-     *     variable wrapper.  Defaults to null which just uses PHP's built-in
-     *     getenv.
-     * @return \Nubs\Which\Locator The locator.
-     */
-    public function create(Environment $environment = null)
-    {
-        return $this->createFromPath(
-            $environment ? $environment->getenv('PATH') : getenv('PATH'),
-            $environment ? $environment->getenv('PATHEXT') : getenv('PATHEXT')
-        );
     }
 }
