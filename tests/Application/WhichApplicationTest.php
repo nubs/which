9d7afc66c155e03870ea34a28500d2ad1296ac19
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2014-07-09 15:09:42 -0400

    Extract common setup for application tests.

--- tests/Application/WhichApplicationTest.php
+++ tests/Application/WhichApplicationTest.php
@@ -9,6 +9,19 @@ use Symfony\Component\Console\Tester\ApplicationTester;
  */
 class WhichApplicationTest extends PHPUnit_Framework_TestCase
 {
+    private $_locator;
+
+    private $_applicationTester;
+
+    public function setUp()
+    {
+        $this->_locator = $this->getMockBuilder('\Nubs\Which\Locator')->disableOriginalConstructor()->setMethods(['locate'])->getMock();
+
+        $application = new WhichApplication($this->_locator);
+        $application->setAutoExit(false);
+        $this->_applicationTester = new ApplicationTester($application);
+    }
+
     /**
      * Verify that a simple call works.
      *
@@ -22,16 +35,11 @@ class WhichApplicationTest extends PHPUnit_Framework_TestCase
      */
     public function simpleCall()
     {
-        $locator = $this->getMockBuilder('\Nubs\Which\Locator')->disableOriginalConstructor()->setMethods(['locate'])->getMock();
-        $locator->expects($this->once())->method('locate')->with('foo')->will($this->returnValue('/path/to/foo'));
+        $this->_locator->expects($this->once())->method('locate')->with('foo')->will($this->returnValue('/path/to/foo'));
 
-        $application = new WhichApplication($locator);
-        $application->setAutoExit(false);
-        $tester = new ApplicationTester($application);
-
-        $tester->run(['commands' => ['foo']]);
-        $this->assertSame(0, $tester->getStatusCode());
-        $this->assertSame("/path/to/foo\n", $tester->getDisplay());
+        $this->_applicationTester->run(['commands' => ['foo']]);
+        $this->assertSame(0, $this->_applicationTester->getStatusCode());
+        $this->assertSame("/path/to/foo\n", $this->_applicationTester->getDisplay());
     }
 
     /**
@@ -47,17 +55,12 @@ class WhichApplicationTest extends PHPUnit_Framework_TestCase
      */
     public function multiCall()
     {
-        $locator = $this->getMockBuilder('\Nubs\Which\Locator')->disableOriginalConstructor()->setMethods(['locate'])->getMock();
-        $locator->expects($this->at(0))->method('locate')->with('foo')->will($this->returnValue('/path/to/foo'));
-        $locator->expects($this->at(1))->method('locate')->with('bar')->will($this->returnValue('/path/to/bar'));
+        $this->_locator->expects($this->at(0))->method('locate')->with('foo')->will($this->returnValue('/path/to/foo'));
+        $this->_locator->expects($this->at(1))->method('locate')->with('bar')->will($this->returnValue('/path/to/bar'));
 
-        $application = new WhichApplication($locator);
-        $application->setAutoExit(false);
-        $tester = new ApplicationTester($application);
-
-        $tester->run(['commands' => ['foo', 'bar']]);
-        $this->assertSame(0, $tester->getStatusCode());
-        $this->assertSame("/path/to/foo\n/path/to/bar\n", $tester->getDisplay());
+        $this->_applicationTester->run(['commands' => ['foo', 'bar']]);
+        $this->assertSame(0, $this->_applicationTester->getStatusCode());
+        $this->assertSame("/path/to/foo\n/path/to/bar\n", $this->_applicationTester->getDisplay());
     }
 
     /**
@@ -73,15 +76,10 @@ class WhichApplicationTest extends PHPUnit_Framework_TestCase
      */
     public function nonexistantCommand()
     {
-        $locator = $this->getMockBuilder('\Nubs\Which\Locator')->disableOriginalConstructor()->setMethods(['locate'])->getMock();
-        $locator->expects($this->once())->method('locate')->with('foo')->will($this->returnValue(null));
+        $this->_locator->expects($this->once())->method('locate')->with('foo')->will($this->returnValue(null));
 
-        $application = new WhichApplication($locator);
-        $application->setAutoExit(false);
-        $tester = new ApplicationTester($application);
-
-        $tester->run(['commands' => ['foo']]);
-        $this->assertSame(1, $tester->getStatusCode());
-        $this->assertSame("foo not found\n", $tester->getDisplay());
+        $this->_applicationTester->run(['commands' => ['foo']]);
+        $this->assertSame(1, $this->_applicationTester->getStatusCode());
+        $this->assertSame("foo not found\n", $this->_applicationTester->getDisplay());
     }
 }
